bc93d2a7c54bba2617ed27fce48b99edc9818800
Caio Bastos <caio.cbastos@hotmail.com>
2020-04-28 17:36:53 -0300

    criando o pacote de dominio

---
+++ src/main/java/com/brq/cursomc/domain/CategoriaDomain.java
@@ -0,0 +1,63 @@
+package com.brq.cursomc.domain;
+
+import java.io.Serializable;
+
+public class CategoriaDomain implements Serializable {
+
+	private static final long serialVersionUID = -4767380303018595905L;
+
+	private Integer id;
+	private String nome;
+	
+	public CategoriaDomain() {
+		
+	}
+
+	public CategoriaDomain(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object objeto) {
+		if (this == objeto)
+			return true;
+		if (objeto == null)
+			return false;
+		if (getClass() != objeto.getClass())
+			return false;
+		CategoriaDomain outro = (CategoriaDomain) objeto;
+		if (id == null) {
+			if (outro.id != null)
+				return false;
+		} else if (!id.equals(outro.id))
+			return false;
+		return true;
+	}
+	
+}
--- src/main/java/com/brq/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/brq/cursomc/resource/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.brq.cursomc.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.brq.cursomc.domain.CategoriaDomain;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	public List<CategoriaDomain> listar() {
+		
+		CategoriaDomain categoriaDomain1 = new CategoriaDomain(1, "Informática");
+		CategoriaDomain categoriaDomain2 = new CategoriaDomain(1, "Escritório");
+		
+		List<CategoriaDomain> listaCategoria = new ArrayList<>();
+		listaCategoria.add(categoriaDomain1);
+		listaCategoria.add(categoriaDomain2);
+		
+		return listaCategoria;
 	}
 	
 }
