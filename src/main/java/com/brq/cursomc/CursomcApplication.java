21ce29f03b5b2ade06c59c2a6e9ce54e9f2742b4
Caio Bastos <caio.cbastos@hotmail.com>
2020-04-29 09:48:49 -0300

    criando operacao de instanciacao

--- src/main/java/com/brq/cursomc/CursomcApplication.java
+++ src/main/java/com/brq/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.brq.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.brq.cursomc.domain.CategoriaDomain;
+import com.brq.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		CategoriaDomain categoriaDomain1 = new CategoriaDomain(null, "Informática");
+		CategoriaDomain categoriaDomain2 = new CategoriaDomain(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(categoriaDomain1, categoriaDomain2));
 	}
 
 }
