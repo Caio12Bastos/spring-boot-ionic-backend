ef2b0db608aac27b16fc99b0a48b6ae11a844a02
Caio Henrique do Carmo Bastos <caiobastos@brq.com>
2020-05-01 18:46:38 -0300

    PUT DELETE GET CLiente

--- src/main/java/com/brq/cursomc/controllers/ClienteController.java
+++ src/main/java/com/brq/cursomc/controllers/ClienteController.java
@@ -1,29 +1,83 @@
 package com.brq.cursomc.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brq.cursomc.domain.ClienteDomain;
+import com.brq.cursomc.dto.ClienteDTO;
 import com.brq.cursomc.services.ClienteService;
 import com.brq.cursomc.services.exception.RecursoNaoEncontrado;
 
 @RestController
-@RequestMapping(value="/clientes")
+@RequestMapping(value = "/clientes")
 public class ClienteController {
 
 	@Autowired
 	private ClienteService clienteService;
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<ClienteDomain> buscarId(@PathVariable Integer id) throws RecursoNaoEncontrado {
-		
+
 		ClienteDomain clienteDomain = clienteService.buscar(id);
-		
+
 		return ResponseEntity.ok().body(clienteDomain);
 	}
-	
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> atualizarCliente(@Valid @RequestBody ClienteDTO clienteDTO,
+			@PathVariable Integer id) {
+		ClienteDomain clienteDomain = clienteService.transformDTO(clienteDTO);
+
+		clienteDomain.setId(id);
+		clienteDomain = clienteService.atualizar(clienteDomain);
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> deletardCliente(@PathVariable Integer id) {
+
+		clienteService.detelar(id);
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> buscarTodasCliente() {
+
+		List<ClienteDomain> listaClienteDomain = clienteService.buscarTodas();
+		List<ClienteDTO> listaClienteDTO = listaClienteDomain.stream()
+				.map(clienteDomain -> new ClienteDTO(clienteDomain)).collect(Collectors.toList());
+
+		return ResponseEntity.ok().body(listaClienteDTO);
+	}
+
+	@RequestMapping(value = "/pagina", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> buscarPaginacao(
+			@RequestParam(value = "pagina", defaultValue = "0") Integer pagina,
+			@RequestParam(value = "linhaPorPagina", defaultValue = "24") Integer linhaPorPagina,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "order", defaultValue = "ASC") String order) {
+
+		Page<ClienteDomain> listaClienteDomain = clienteService.buscaPaginacao(pagina, linhaPorPagina, orderBy,
+				order);
+
+		Page<ClienteDTO> listaClienteDTO = listaClienteDomain
+				.map(clienteDomain -> new ClienteDTO(clienteDomain));
+
+		return ResponseEntity.ok().body(listaClienteDTO);
+	}
+
 }
--- src/main/java/com/brq/cursomc/services/ClienteService.java
+++ src/main/java/com/brq/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.brq.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.brq.cursomc.domain.ClienteDomain;
+import com.brq.cursomc.dto.ClienteDTO;
 import com.brq.cursomc.repositories.ClienteRepository;
+import com.brq.cursomc.services.exception.DataIntegrityException;
 import com.brq.cursomc.services.exception.RecursoNaoEncontrado;
 
 @Service
@@ -14,12 +21,47 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	public ClienteDomain buscar(Integer id) throws RecursoNaoEncontrado {
-		
+
 		Optional<ClienteDomain> optClienteDomain = clienteRepository.findById(id);
-		return optClienteDomain.orElseThrow(() -> new RecursoNaoEncontrado( 
-				"Objeto não encontrado! Id: " + id)); 
+		return optClienteDomain.orElseThrow(() -> new RecursoNaoEncontrado("Objeto não encontrado! Id: " + id));
 	}
-	
+
+	public ClienteDomain atualizar(ClienteDomain cliente) {
+
+		ClienteDomain novoClienteDomain = buscar(cliente.getId());
+		atualizaDados(novoClienteDomain, cliente);
+		return clienteRepository.save(novoClienteDomain);
+	}
+
+	private void atualizaDados(ClienteDomain novoClienteDomain, ClienteDomain cliente) {
+		novoClienteDomain.setNome(cliente.getNome());
+		novoClienteDomain.setEmail(cliente.getEmail());
+	}
+
+	public void detelar(Integer id) {
+
+		buscar(id);
+		try {
+			clienteRepository.deleteById(id);
+		} catch (DataIntegrityViolationException excecao) {
+			throw new DataIntegrityException("Não é possível excluir porque o cliente contêm pedido.");
+		}
+	}
+
+	public List<ClienteDomain> buscarTodas() {
+		return clienteRepository.findAll();
+	}
+
+	public Page<ClienteDomain> buscaPaginacao(Integer pagina, Integer linhaPorPagina, String orderBy, String order) {
+
+		PageRequest paginaRequest = PageRequest.of(pagina, linhaPorPagina, Direction.valueOf(order), orderBy);
+		return clienteRepository.findAll(paginaRequest);
+	}
+
+	public ClienteDomain transformDTO(ClienteDTO clienteDTO) {
+		return new ClienteDomain(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+	}
+
 }
