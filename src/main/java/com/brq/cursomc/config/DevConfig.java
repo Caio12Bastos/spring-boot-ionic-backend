d89dd6378394b62c862c106956fd91947ed00e07
Caio Henrique do Carmo Bastos <caiobastos@brq.com>
2020-05-04 16:30:26 -0300

    criando profices de dev

---
+++ src/main/java/com/brq/cursomc/config/DevConfig.java
@@ -0,0 +1,36 @@
+package com.brq.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.brq.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	private static final Object CREATE = "create";
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String stategy;
+	
+	@Bean
+	public boolean instantiateDataBase() throws ParseException {
+		
+		if(!CREATE.equals(stategy)) {
+			return false;
+		}
+		
+		dbService.instantiateDataBase();
+		return true;
+	}
+	
+}
